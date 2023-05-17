0324a5d3099c6afec85229cd7cf2e31bf4afddee
Tarsis <br.tmvdl@gmail.com>
2023-05-16 21:35:51 -0300

    Fixing projects files

--- src/www/index.js
+++ src/www/index.js
@@ -9,6 +9,11 @@ const __ = {
   getTextSize: () => (1 / 2),
   getTextHeight: () => 1 / 50,
   getAngle: (angle) => 2 * Math.PI * angle,
+  getDate: (date = new Date()) => ({
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds(),
+  }),
   fonts: {
     HELVETIKER: './libs/three/examples/fonts/helvetiker_regular.typeface.json'
   }
@@ -16,89 +21,101 @@ const __ = {
 
 const COLORS = {
   WHITE: 'rgb(255, 255, 255)',
-  WHITE1: 'rgb(204, 204, 204)',
   GRAY: 'rgb(153, 153, 153)',
-  GRAY1: 'rgb(102, 102, 102)',
-  BLACK1: 'rgb(51, 51, 51)',
   BLACK: 'rgb(0, 0, 0)',
 }
 
+const loader = new FontLoader()
+const fonts = {}
+
+const loadFont = (name) => new Promise((res, rej) => fonts[name]
+  ? res(fonts[name])
+  : loader.load(
+    name,
+    (font) => res(fonts[name] = font),
+    undefined,
+    (err) => rej(err),
+  )
+)
+
 const scene = new THREE.Scene()
+scene.background = (new THREE.Color(COLORS.WHITE))
 
 const clock = new THREE.Mesh(
-  new THREE.CylinderGeometry(Math.PI * 18 / 12, 2.75, 0.999),
-  new THREE.MeshBasicMaterial({ color: COLORS.BLACK1 })
+  new THREE.CylinderGeometry(+Math.PI * +1.5, +2.75, +0.999),
+  new THREE.MeshBasicMaterial({ color: COLORS.BLACK })
 )
 scene.add(clock)
 
 const edge = new THREE.Mesh(
-  new THREE.CylinderGeometry(+Math.PI / +0.75, +2.75, +1.0),
+  new THREE.CylinderGeometry(+4.0, +2.75, +1.0),
   new THREE.MeshBasicMaterial({ color: COLORS.WHITE })
 )
 clock.add(edge)
 
 const origin = new THREE.Mesh(
-  new THREE.CylinderGeometry(+Math.PI / +16.0, +0.1, +1.0),
-  new THREE.MeshBasicMaterial({ color: COLORS.BLACK1 })
+  new THREE.CylinderGeometry(+0.2, +0.1, +1.0),
+  new THREE.MeshBasicMaterial({ color: COLORS.BLACK })
 )
-origin.position.set(+0.0, +0.4, +0.0)
+origin.position.set(+0.0, +0.5, +0.0)
 edge.add(origin)
 
-const hourPointer = new THREE.Mesh(
-  new THREE.CylinderGeometry(.05, .01, 4),
-  new THREE.MeshBasicMaterial({ color: COLORS.GRAY }),
-)
-hourPointer.rotation.set(0, 0, -Math.PI / 2)
-hourPointer.position.set(0, 0.75, 0)
-edge.add(hourPointer)
+const [
+  hourPointer,
+  minutePointer,
+  secondPointer,
+] = Array.from(Array(3)).map(() => {
+  const p = new THREE.Mesh(
+    new THREE.CylinderGeometry(+0.05, +0.01, +4.0),
+    new THREE.MeshBasicMaterial({ color: new THREE.Color(0xCCCCCC) })
+  )
 
-const minutePointer = new THREE.Mesh(
-  new THREE.CylinderGeometry(.05, .01, 4),
-  new THREE.MeshBasicMaterial({ color: COLORS.GRAY1 })
-)
-minutePointer.rotation.set(0, 0, -Math.PI / 2)
-minutePointer.position.set(0, 0.75, 0)
-edge.add(minutePointer)
+  p.rotation.set(+0.0, +0.0, -Math.PI / +2.0)
+  p.position.set(+0.0, +0.75, +0.0)
 
-const secondPointer = new THREE.Mesh(
-  new THREE.CylinderGeometry(.05, .01, 4),
-  new THREE.MeshBasicMaterial({ color: COLORS.WHITE1 })
-)
-secondPointer.rotation.set(0, 0, -Math.PI / 2)
-secondPointer.position.set(0, 0.75, 0)
-edge.add(secondPointer)
+  edge.add(p)
 
-const fonts = {}
-
-const loadFont = (name) => new Promise((res, rej) => {
-  if (fonts[name]) return res(fonts[name])
-  const loader = new FontLoader()
-  loader.load(name, (font) => res(font), () => { }, (err) => rej(err))
+  return p
+}).map((p, ix) => {
+  const [r, g, b,] = p.material.color.toArray()
+  p.material.color.fromArray([(r / ix), (g / ix), (b / ix),])
+  return p
 })
 
-Array.from(Array(12))
-  .forEach((_, angle) => loadFont(__.fonts.HELVETIKER)
-    .then((font) => {
-      const textGeometry = new TextGeometry((angle == 0 ? '12' : angle).toString(), {
-        font,
-        size: __.getTextSize(),
-        height: __.getTextHeight(),
-      })
-      textGeometry.center()
+const nArray = []
+const nGroup = new THREE.Group()
+clock.add(nGroup)
 
-      const mesh = new THREE.Mesh(
-        textGeometry,
-        new THREE.MeshBasicMaterial({ color: COLORS.BLACK1 })
-      )
-      mesh.rotation.set(+Math.PI * +1.5, 0, -Math.PI * +0.5)
+function showLetters(arr = []) {
+  nArray.map((mesh) => nGroup.remove(mesh))
 
-      const hyp = Math.PI
-      mesh.position.x = hyp * Math.cos(angle * Math.PI / +6.0)
-      mesh.position.y = +0.75
-      mesh.position.z = hyp * Math.sin(angle * Math.PI / +6.0)
+  return arr
+    .map((text, angle) => loadFont(__.fonts.HELVETIKER)
+      .then((font) => {
+        const textGeometry = new TextGeometry(text, {
+          font,
+          size: __.getTextSize(),
+          height: __.getTextHeight(),
+        })
+        textGeometry.center()
 
-      clock.add(mesh)
-    }))
+        const mesh = new THREE.Mesh(
+          textGeometry,
+          new THREE.MeshBasicMaterial({ color: COLORS.BLACK })
+        )
+        mesh.rotation.x = +Math.PI * +1.5
+        mesh.rotation.z = -Math.PI * +0.5
+
+        const hyp = Math.PI
+        mesh.position.x = hyp * Math.cos(angle * Math.PI / +6.0)
+        mesh.position.y = +0.5
+        mesh.position.z = hyp * Math.sin(angle * Math.PI / +6.0)
+
+        nGroup.add(mesh)
+        nArray.push(mesh)
+      }))
+}
+
 
 // // //
 // // //
@@ -114,15 +131,29 @@ document.body.appendChild(renderer.domElement)
 document.body.style.margin = '0rem'
 
 renderer.setAnimationLoop(() => {
-  const date = new Date()
+  const { hours, minutes, seconds } = __.getDate()
 
-  const hourRotation = __.getAngle(-date.getHours() / 12)
-  const minuteRotation = __.getAngle(-date.getMinutes() / 60)
-  const secondRotation = __.getAngle(-date.getSeconds() / 60)
-
-  hourPointer.rotation.y = hourRotation
-  minutePointer.rotation.y = minuteRotation
-  secondPointer.rotation.y = secondRotation
+  hourPointer.rotation.y = __.getAngle(-hours / 12)
+  minutePointer.rotation.y = __.getAngle(-minutes / 60)
+  secondPointer.rotation.y = __.getAngle(-seconds / 60)
 
   renderer.render(scene, camera)
 })
+
+function showNumbers() {
+  showLetters(['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'])
+}
+
+function showRomans() {
+  showLetters(['XII', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI'])
+}
+
+window.addEventListener('keypress', ({ key }) => {
+  switch (key) {
+    case 'a': return showNumbers()
+    case 's': return showRomans()
+    case 'd': return showLetters([])
+  }
+})
+
+showNumbers()
