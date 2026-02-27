27bf13af0d1b7b5bfffb6ced00bd5823e73bd543
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-02-27 10:54:50 -0300

    Update project files

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 import * as THREE from './libs/three/index.js'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
 import { FontLoader } from 'three/addons/loaders/FontLoader.js'
 
 import { getWidth, getHeight } from './utils/window.js'
 import { createCamera } from './utils/3d.js'
-import { padLeft } from './utils/str.js'
 
 const helvetiker = './fonts/helvetiker_regular.typeface.json'
 
 const scene = new THREE.Scene()
 const camera = createCamera()
-camera.position.set(-10.0, +20.0, +0.0)
+camera.position.set(+0.0, +10.0, +0.0)
+camera.lookAt(+0.0, +0.0, +0.0)
+camera.rotation.z = -Math.PI / 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setPixelRatio(window.devicePixelRatio)
@@ -19,51 +19,67 @@ renderer.setSize(getWidth(), getHeight())
 document.body.appendChild(renderer.domElement)
 document.body.style.margin = '0em'
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.update()
+const mainGroup = new THREE.Group()
+scene.add(mainGroup)
 
 const origin = new THREE.Mesh(
   new THREE.CylinderGeometry(Math.PI / 16, .1, 1),
   new THREE.MeshBasicMaterial({ color: 0x333333 })
 )
-origin.position.set(0, 2 / 5, 0)
-scene.add(origin)
+origin.position.set(0, 0.5, 0)
+mainGroup.add(origin)
 
 const clock = new THREE.Mesh(
-  new THREE.CylinderGeometry(Math.PI * 18 / 12, 2.75, 0.999),
+  new THREE.CylinderGeometry(5, 5, 1, 2 ** 8),
   new THREE.MeshBasicMaterial({ color: 0x333333 })
 )
-scene.add(clock)
+mainGroup.add(clock)
 
 const edge = new THREE.Mesh(
-  new THREE.CylinderGeometry(Math.PI * 16 / 12, 2.75, 1),
+  new THREE.CylinderGeometry(4, 4, 1, 2 ** 8),
   new THREE.MeshBasicMaterial({ color: 0xFFFFFF })
 )
+edge.position.set(0, 0.1, 0)
 clock.add(edge)
 
+const hourPointerGroup = new THREE.Group()
+hourPointerGroup.rotation.set(0, 0, -Math.PI / 2)
+hourPointerGroup.position.set(0, 0.75, 0)
+mainGroup.add(hourPointerGroup)
+
 const hourPointer = new THREE.Mesh(
-  new THREE.CylinderGeometry(.05, .01, 4),
+  new THREE.CylinderGeometry(.03, .03, 2),
   new THREE.MeshBasicMaterial({ color: 0x666666 }),
 )
-hourPointer.rotation.set(0, 0, -Math.PI / 2)
-hourPointer.position.set(0, 0.75, 0)
-scene.add(hourPointer)
+hourPointer.position.x = -1 * 1 / 4
+hourPointer.position.y = 1.2
+hourPointerGroup.add(hourPointer)
+
+const minutePointerGroup = new THREE.Group()
+minutePointerGroup.rotation.set(0, 0, -Math.PI / 2)
+minutePointerGroup.position.set(0, 0.75, 0)
+mainGroup.add(minutePointerGroup)
 
 const minutePointer = new THREE.Mesh(
-  new THREE.CylinderGeometry(.05, .01, 4),
+  new THREE.CylinderGeometry(.03, .03, 2.5),
   new THREE.MeshBasicMaterial({ color: 0x999999 })
 )
-minutePointer.rotation.set(0, 0, -Math.PI / 2)
-minutePointer.position.set(0, 0.75, 0)
-scene.add(minutePointer)
+minutePointer.position.x = -1 * 1 / 8
+minutePointer.position.y = 1.2
+minutePointerGroup.add(minutePointer)
+
+const secondPointerGroup = new THREE.Group()
+secondPointerGroup.rotation.set(0, 0, -Math.PI / 2)
+secondPointerGroup.position.set(0, 0.75, 0)
+mainGroup.add(secondPointerGroup)
 
 const secondPointer = new THREE.Mesh(
-  new THREE.CylinderGeometry(.05, .01, 4),
+  new THREE.CylinderGeometry(.03, .03, 3),
   new THREE.MeshBasicMaterial({ color: 0xcccccc })
 )
-secondPointer.rotation.set(0, 0, -Math.PI / 2)
-secondPointer.position.set(0, 0.75, 0)
-scene.add(secondPointer)
+secondPointer.position.x = -1 * 1 / 16
+secondPointer.position.y = 1.2
+secondPointerGroup.add(secondPointer)
 
 const fonts = {}
 
@@ -103,29 +119,9 @@ const degreeAngle = (angle) => 2 * Math.PI * angle
 renderer.setAnimationLoop(() => {
   const date = new Date()
 
-  hourPointer.rotation.y = degreeAngle(-date.getHours() / 12)
-  minutePointer.rotation.y = degreeAngle(-date.getMinutes() / 60)
-  secondPointer.rotation.y = degreeAngle(-date.getSeconds() / 60)
+  hourPointerGroup.rotation.y = degreeAngle(-date.getHours() / 12)
+  minutePointerGroup.rotation.y = degreeAngle(-date.getMinutes() / 60)
+  secondPointerGroup.rotation.y = degreeAngle(-date.getSeconds() / 60)
 
-  controls.update()
   renderer.render(scene, camera)
 })
-
-
-const title = document.getElementsByTagName('title').item(0)
-
-window.setInterval(() => {
-  const date = new Date()
-
-  const yearDate = date.getFullYear().toString()
-  const monthDate = padLeft(date.getMonth() + 1, 2, '0').toString()
-  const dayDate = padLeft(date.getDay(), 2, '0').toString()
-
-  const hourDate = padLeft(date.getHours(), 2, '0').toString()
-  const minuteDate = padLeft(date.getMinutes(), 2, '0').toString()
-  const secondsDate = padLeft(date.getSeconds(), 2, '0').toString()
-
-  const dateString = `${yearDate}/${monthDate}/${dayDate} ${hourDate}:${minuteDate}:${secondsDate}`
-
-  title.textContent = ['Clock', dateString].join(' - ')
-}, 1e3)
